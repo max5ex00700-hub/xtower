a342b2a7404f87a792f061d6ada7ba09a41fb767
max5ex00700-hub <max5ex00700@gmail.com>
2026-09-21 20:31:09 +0900

    Bump build for floor character lock test

--- unity/XTapUnity/Assets/Editor/XTapBuildConfig.cs
+++ unity/XTapUnity/Assets/Editor/XTapBuildConfig.cs
@@ -12,12 +12,12 @@ public sealed class XTapBuildConfig : IPreprocessBuildWithReport
     {
         PlayerSettings.productName = "X탑";
         PlayerSettings.companyName = "XTap";
-        PlayerSettings.bundleVersion = "10.32-gacha-test1";
+        PlayerSettings.bundleVersion = "10.33-floor1-lock1";
         PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.xtower.game.unity");
         PlayerSettings.defaultInterfaceOrientation = UIOrientation.Portrait;
         PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel24;
         PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
-        PlayerSettings.Android.bundleVersionCode = 1032;
+        PlayerSettings.Android.bundleVersionCode = 1033;
 
         // 64-bit Android is required for current 64-bit-only devices.
         PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
