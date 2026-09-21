a1a47e1f9d262c493518edc5df4338b642d0a21f
max5ex00700-hub <max5ex00700@gmail.com>
2026-09-21 21:45:09 +0900

    Bump build to 10.36 touch input fix

--- unity/XTapUnity/Assets/Editor/XTapBuildConfig.cs
+++ unity/XTapUnity/Assets/Editor/XTapBuildConfig.cs
@@ -12,12 +12,12 @@ public sealed class XTapBuildConfig : IPreprocessBuildWithReport
     {
         PlayerSettings.productName = "X탑";
         PlayerSettings.companyName = "XTap";
-        PlayerSettings.bundleVersion = "10.35-gothic-main1";
+        PlayerSettings.bundleVersion = "10.36-main-touchfix1";
         PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.xtower.game.unity");
         PlayerSettings.defaultInterfaceOrientation = UIOrientation.Portrait;
         PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel24;
         PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
-        PlayerSettings.Android.bundleVersionCode = 1035;
+        PlayerSettings.Android.bundleVersionCode = 1036;
 
         // 64-bit Android is required for current 64-bit-only devices.
         PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
