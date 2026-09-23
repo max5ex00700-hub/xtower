044c3b4912fb8f960fbd6dcf5e3c2bfde73643ea
max5ex00700-hub <max5ex00700@gmail.com>
2026-09-23 16:06:12 +0900

    Bump build to 10.71 block drag alignment

--- unity/XTapUnity/Assets/Editor/XTapBuildConfig.cs
+++ unity/XTapUnity/Assets/Editor/XTapBuildConfig.cs
@@ -12,12 +12,12 @@ public sealed class XTapBuildConfig : IPreprocessBuildWithReport
     {
         PlayerSettings.productName = "X탑";
         PlayerSettings.companyName = "XTap";
-        PlayerSettings.bundleVersion = "10.70-forge-shared-storage1";
+        PlayerSettings.bundleVersion = "10.71-block-drag-align1";
         PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.xtower.game.unity");
         PlayerSettings.defaultInterfaceOrientation = UIOrientation.Portrait;
         PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel24;
         PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
-        PlayerSettings.Android.bundleVersionCode = 1070;
+        PlayerSettings.Android.bundleVersionCode = 1071;
 
         // 64-bit Android is required for current 64-bit-only devices.
         PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
