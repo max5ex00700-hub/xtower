0ceab77ddb1936b131d603dcfc8f4a9bdb7b4d12
max5ex00700-hub <max5ex00700@gmail.com>
2026-09-23 11:35:33 +0900

    Bump build to 10.61 blacksmith art

--- unity/XTapUnity/Assets/Editor/XTapBuildConfig.cs
+++ unity/XTapUnity/Assets/Editor/XTapBuildConfig.cs
@@ -12,12 +12,12 @@ public sealed class XTapBuildConfig : IPreprocessBuildWithReport
     {
         PlayerSettings.productName = "X탑";
         PlayerSettings.companyName = "XTap";
-        PlayerSettings.bundleVersion = "10.60-big-stats1";
+        PlayerSettings.bundleVersion = "10.61-blacksmith-art1";
         PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.xtower.game.unity");
         PlayerSettings.defaultInterfaceOrientation = UIOrientation.Portrait;
         PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel24;
         PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
-        PlayerSettings.Android.bundleVersionCode = 1060;
+        PlayerSettings.Android.bundleVersionCode = 1061;
 
         // 64-bit Android is required for current 64-bit-only devices.
         PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
