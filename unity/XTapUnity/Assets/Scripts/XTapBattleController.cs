e1964bfceb3f2518675215988b93fe285bc31bed
max5ex00700-hub <max5ex00700@gmail.com>
2026-09-25 13:01:10 +0900

    Sync options version code to 1114

--- unity/XTapUnity/Assets/Scripts/XTapBattleController.cs
+++ unity/XTapUnity/Assets/Scripts/XTapBattleController.cs
@@ -759,7 +759,7 @@ public sealed class XTapBattleController : MonoBehaviour
         Anchor(bgmButton.GetComponent<RectTransform>(), .08f, .24f, .92f, .40f);
         bgmButton.onClick.AddListener(ToggleBgmSetting);
 
-        Text version = MakeOutlinedText(panel.transform, "버전 정보   " + Application.version + "  (1113)", 12, TextAnchor.MiddleCenter, true);
+        Text version = MakeOutlinedText(panel.transform, "버전 정보   " + Application.version + "  (1114)", 12, TextAnchor.MiddleCenter, true);
         version.color = new Color(.72f, .69f, .64f, 1f);
         Anchor(version.rectTransform, .08f, .12f, .92f, .22f);
 
